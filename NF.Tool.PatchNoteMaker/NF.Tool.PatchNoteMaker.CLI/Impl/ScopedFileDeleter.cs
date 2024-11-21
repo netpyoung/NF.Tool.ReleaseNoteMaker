5054c7b0a8d93c9fadc25a72d8babc1f95ca1e1a
netpyoung <netpyoung@gmail.com>
2024-11-21 21:48:09 +0900

    add: ScopedFileDeleter.cs

---
+++ NF.Tool.PatchNoteMaker/NF.Tool.PatchNoteMaker.CLI/Impl/ScopedFileDeleter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NF.Tool.PatchNoteMaker.CLI.Impl
+{
+    public record struct ScopedFileDeleter : IDisposable
+    {
+        private readonly List<string> _willDeleteFilePaths = new List<string>(20);
+        public ScopedFileDeleter()
+        {
+        }
+
+        public string Register(string path)
+        {
+            _willDeleteFilePaths.Add(path);
+            return path;
+        }
+
+        public void Dispose()
+        {
+            foreach (string path in _willDeleteFilePaths)
+            {
+                File.Delete(path);
+            }
+            _willDeleteFilePaths.Clear();
+        }
+    }
+}
