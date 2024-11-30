b0dc42fa9274a9d66828a702d2b60d68f648d96d
netpyoung <netpyoung@gmail.com>
2024-11-30 10:37:43 +0900

    test: TestCommand

---
+++ NF.Tool.ReleaseNoteMaker/NF.Tool.ReleaseNoteMaker.Tests/TestBuild.cs
@@ -0,0 +1,21 @@
+﻿using NF.Tool.ReleaseNoteMaker.CLI;
+
+namespace NF.Tool.ReleaseNoteMaker.Tests
+{
+    [TestClass]
+    [DoNotParallelize]
+    public class TestBuild
+    {
+
+        [TestMethod]
+        [DeploymentItem("SampleData/Case001", "ChangeLog.d/")]
+        [DeploymentItem("Template.tt")]
+        [DeploymentItem("ReleaseNote.config.toml")]
+        public async Task TestCommand()
+        {
+            string[] args = ["build", "--draft", "--date", "01-01-2001", "--version", "1.0.0", "--name", "HelloWorld"];
+            int result = await Program.Main(args);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
