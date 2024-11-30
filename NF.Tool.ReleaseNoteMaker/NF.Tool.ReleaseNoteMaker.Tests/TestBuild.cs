75abee613da449dcac6130b0502e925208e055e9
netpyoung <netpyoung@gmail.com>
2024-11-30 11:24:37 +0900

    test: update TestCommand

--- NF.Tool.ReleaseNoteMaker/NF.Tool.ReleaseNoteMaker.Tests/TestBuild.cs
+++ NF.Tool.ReleaseNoteMaker/NF.Tool.ReleaseNoteMaker.Tests/TestBuild.cs
@@ -1,4 +1,6 @@
 ﻿using NF.Tool.ReleaseNoteMaker.CLI;
+using Spectre.Console;
+using Spectre.Console.Testing;
 
 namespace NF.Tool.ReleaseNoteMaker.Tests
 {
@@ -13,9 +15,39 @@ namespace NF.Tool.ReleaseNoteMaker.Tests
         [DeploymentItem("ReleaseNote.config.toml")]
         public async Task TestCommand()
         {
+            TestConsole c = new TestConsole();
+            AnsiConsole.Console = c;
+
             string[] args = ["build", "--draft", "--date", "01-01-2001", "--version", "1.0.0", "--name", "HelloWorld"];
             int result = await Program.Main(args);
             Assert.AreEqual(0, result);
+
+            string expected = @"* Finding news fragments...
+* Loading template...
+* Rendering news fragments...
+* show draft...
+# HelloWorld 1.0.0 (01-01-2001)
+
+
+## Main
+
+### Features
+
+- Baz levitation (baz)
+- Baz fix levitation (fix-1.2)
+- Adds levitation (#123)
+- Extends levitation (#124)
+- An orphaned feature ending with a dotted number
+- An orphaned feature ending with a number
+- An orphaned feature starting with a dotted number
+- An orphaned feature starting with a number
+- Another orphaned feature
+- Orphaned feature
+
+
+
+";
+            Assert.AreEqual(expected.Replace(Environment.NewLine, "\n"), c.Output);
         }
     }
 }
