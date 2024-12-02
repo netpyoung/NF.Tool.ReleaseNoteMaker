db8d777d81a0d331de56e8b0d5211271b1b39186
netpyoung <netpyoung@gmail.com>
2024-12-02 11:41:13 +0900

    refactor: split Cmd.Call

--- NF.Tool.ReleaseNoteMaker/NF.Tool.ReleaseNoteMaker.CLI/Impl/GitHelper.cs
+++ NF.Tool.ReleaseNoteMaker/NF.Tool.ReleaseNoteMaker.CLI/Impl/GitHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -9,7 +10,7 @@ namespace NF.Tool.ReleaseNoteMaker.CLI.Impl
 
         public static void StageNewsfile(string fpath)
         {
-            _ = Call("git", $@"add ""{fpath}""");
+            _ = Cmd.Call("git", $@"add ""{fpath}""");
         }
 
         public static void RemoveFiles(string[] fragmentFpaths)
@@ -19,9 +20,9 @@ namespace NF.Tool.ReleaseNoteMaker.CLI.Impl
                 return;
             }
 
-            string[] gitFragmentFpaths = Call("git", $"ls-files {string.Join(' ', fragmentFpaths)}").Split("\n");
+            string[] gitFragmentFpaths = Cmd.Call("git", $"ls-files {string.Join(' ', fragmentFpaths)}").Split("\n");
             gitFragmentFpaths = gitFragmentFpaths.Where(File.Exists).Select(Path.GetFullPath).ToArray();
-            _ = Call("git", $"rm --quiet --force {string.Join(' ', gitFragmentFpaths)}");
+            _ = Cmd.Call("git", $"rm --quiet --force {string.Join(' ', gitFragmentFpaths)}");
 
             string[] unknownFragmentFpaths = fragmentFpaths.Except(gitFragmentFpaths).ToArray();
             foreach (string x in unknownFragmentFpaths)
@@ -29,7 +30,10 @@ namespace NF.Tool.ReleaseNoteMaker.CLI.Impl
                 File.Delete(x);
             }
         }
+    }
 
+    internal static class Cmd
+    {
         public static string Call(string fileName, string arguments)
         {
 #pragma warning disable CA1031 // Do not catch general exception types
@@ -59,6 +63,40 @@ namespace NF.Tool.ReleaseNoteMaker.CLI.Impl
             catch
             {
                 return string.Empty;
+            }
+#pragma warning restore CA1031 // Do not catch general exception types
+        }
+
+        public static (Exception? exOrNull, int exitCode, string stdOut, string stdErr) Call2(string fileName, string arguments)
+        {
+#pragma warning disable CA1031 // Do not catch general exception types
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = fileName,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = processStartInfo;
+
+                    _ = process.Start();
+                    string stdOut = process.StandardOutput.ReadToEnd();
+                    string stdErr = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+                    int exitCode = process.ExitCode;
+                    return (null, exitCode, stdOut, stdErr);
+                }
+            }
+            catch (Exception ex)
+            {
+                return (ex, 0, string.Empty, string.Empty);
             }
 #pragma warning restore CA1031 // Do not catch general exception types
         }
