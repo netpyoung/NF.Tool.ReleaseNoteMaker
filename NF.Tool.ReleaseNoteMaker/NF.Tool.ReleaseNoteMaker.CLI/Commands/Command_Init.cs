3980c961869794b69bc01e7d5637e73570f2111c
netpyoung <netpyoung@gmail.com>
2025-11-16 22:13:38 +0900

    change: `dotnet release-note init` command will create CHANGELOG.md

--- NF.Tool.ReleaseNoteMaker/NF.Tool.ReleaseNoteMaker.CLI/Commands/Command_Init.cs
+++ NF.Tool.ReleaseNoteMaker/NF.Tool.ReleaseNoteMaker.CLI/Commands/Command_Init.cs
@@ -1,8 +1,10 @@
 ﻿using NF.Tool.ReleaseNoteMaker.CLI.Impl;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using System;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -74,12 +76,36 @@ namespace NF.Tool.ReleaseNoteMaker.CLI.Commands
             string templateFileTempPath = Utils.ExtractResourceToTempFilePath(templateFileName);
             File.Move(templateFileTempPath, templatePath);
 
+            bool isNeedToCreateChagelogFile = true;
+            {
+                // CHANGELOG.md
+                foreach (string file in Directory.EnumerateFiles(Directory.GetCurrentDirectory()))
+                {
+                    if (string.Equals(Path.GetFileName(file), "CHANGELOG.md", StringComparison.OrdinalIgnoreCase))
+                    {
+                        isNeedToCreateChagelogFile = false;
+                        break;
+                    }
+                }
+
+                if (isNeedToCreateChagelogFile)
+                {
+                    string chagelogFileTempPath = Utils.ExtractResourceToTempFilePath("CHANGELOG.md");
+                    File.Move(chagelogFileTempPath, "CHANGELOG.md");
+                }
+            }
+
             {
                 // display layout
 
                 AnsiConsole.WriteLine("Initialized");
                 Tree root = new Tree("./");
                 _ = root.AddNode($"{Const.DEFAULT_CONFIG_FILENAME}");
+                if (isNeedToCreateChagelogFile)
+                {
+                    _ = root.AddNode("CHANGELOG.md");
+                }
+
                 TreeNode changelogD = root.AddNode("[blue]ChangeLog.d/[/]");
                 _ = changelogD.AddNode($"{templateFileName}");
                 AnsiConsole.Write(root);
